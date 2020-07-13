703a1fd2887e6095c0e425c0722847196697ffb5
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-07-13 20:34:13 -0300

    add data service

---
+++ src/services/data.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    avatar: "https://api.adorable.io/avatars/285/abott@adorable.png",
+    name: "Iarlen Aquiles",
+    company: "Facebook",
+    distance: 10,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    avatar: "https://api.adorable.io/avatars/285/abott@adorable.png",
+    name: "Ronaldo",
+    company: "Youtube",
+    distance: 2500,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  },
+  {
+    avatar: "https://api.adorable.io/avatars/285/abott@adorable.png",
+    name: "",
+    company: "Adriana",
+    distance: 56,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  }
+];
