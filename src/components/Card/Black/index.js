532e8df63c8720d12d0660840da33b9fa7f266cf
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-07-14 08:54:05 -0300

    added starred black

---
+++ src/components/Card/Black/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import * as S from "./styles";
+
+const Black = () => {
+  return (
+    <S.Stared>
+      <S.Text>Lorem Ipsum</S.Text>
+    </S.Stared>
+  );
+};
+
+export default Black;
